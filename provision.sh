31d8970fe85222cdc371c8115c50bcc5dbb58dbb
John Knoernschild <procisco@gmail.com>
2022-02-08 20:03:47 -0500

    changed from sybolic link to file copy from /vagrant/sites-enabled

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@ apt-get -y update
 
 apt-get -y install nginx
 
-rm -rf /var/www/html/
-ln -s /vagrant/html /var/www
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
 
-service nginx start
+service nginx restart
